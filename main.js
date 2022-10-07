08f33b7c8f0a03ed004f6af115629904df211a33
Nicholas Adams <njadams.722@gmail.com>
2022-10-07 10:50:44 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const quarterValue = (inputNumber) => {
+    
+    const quartered = inputNumber / 4
+    return quartered
+}
+
+// Invoke the function and store the return value
+const result = quarterValue(18)
+// Log the return value
+console.log(result)
